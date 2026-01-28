85adb9332a3ed43bb1a92541746004bbfa5cf0f4
Christian Alexander Albarracin Adriano <christianalex-071@hotmail.com>
2026-01-28 17:10:32 -0500

    Initial commit - TestApi minimal

---
+++ TestApi/Program.cs
@@ -0,0 +1,22 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+app.MapOpenApi();
+
+// Health endpoint
+app.MapGet("/health", () => new 
+{
+    status = "healthy",
+    timestamp = DateTime.UtcNow,
+    app = "TestApi"
+});
+
+// Simple hello endpoint
+app.MapGet("/hello", () => new 
+{
+    message = "Hello from TestApi!",
+    timestamp = DateTime.UtcNow
+});
+
+app.Run();
